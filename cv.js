22691e5e9c0950bc8d3baaeb90a3576336dfe9db
abu-pixel <jinkizkhan100@gmail.com>
2024-10-14 11:17:03 -0700

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const elementsToAnimate = document.querySelectorAll('.fade-in, .slide-in');
+
+    const observerOptions = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    elementsToAnimate.forEach(element => {
+        observer.observe(element);
+    });
+});
